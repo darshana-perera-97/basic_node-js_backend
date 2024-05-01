655a3a058cbb08bf551a349dede48c8afd8ba03e
Darshana Perera <ds.perera.1997@gmail.com>
2024-05-01 20:05:33 +0530

    new api added

--- index.js
+++ index.js
@@ -12,10 +12,16 @@ const books = [
   { id: 2, title: "Book 2", author: "Author 2" },
   { id: 3, title: "Book 3", author: "Author 3" },
 ];
+const books2 = [
+  { id: 1, title: "Book 1", author: "Author 1" },
+];
 
 // Routes
 app.get("/api", (req, res) => {
   res.json(books);
+});
+app.get("/", (req, res) => {
+  res.json(books2);
 });
 
 app.get("/api/books/:id", (req, res) => {
